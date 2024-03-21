5e94f1026c6d88923fc59e1a4b5ae5308c1bb7b3
Ayflow350 <soluwatist@gmail.com>
2024-03-21 11:07:09 +0100

    first comm

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,6 @@
+
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from 'next-auth/providers/github';
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
 import NextAuth from "next-auth/next";
@@ -6,6 +9,16 @@ import bcrypt from "bcryptjs";
 
 export const authOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+  }),
+  GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+  }),
+
+
     CredentialsProvider({
       name: "credentials",
       credentials: {},
