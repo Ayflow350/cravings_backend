ed702da2962e88a913ceeab409fd66c9df91739e
Ayflow350 <soluwatist@gmail.com>
2024-03-21 03:46:20 +0100

    first commit

---
+++ components/LoginForm.jsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
+
+      router.replace("dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="w-full max-w-md space-y-8">
+      <div>
+        <img
+          className="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign In to cravings
+        </h2>
+       
+      </div>
+    
+      <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <input type="hidden" name="remember" defaultValue="true" />
+        <div className="space-y-[24px] rounded-md shadow-sm">
+         
+          <div>
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
+       
+        <button className="bg-indigo-600 text-white font-bold cursor-pointer px-6 py-2">
+            Login
+          </button>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+
+          <Link className="text-sm mt-3 text-right" href={"/register"}>
+            Don't have an account? <span className="underline">Register</span>
+          </Link>
+
+        
+      </form>
+    </div>
+  </div>
+    </>
+  );
+}
